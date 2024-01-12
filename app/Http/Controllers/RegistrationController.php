4aff573f50e1f072aa15eefc2ca6e2946e1cd2e6
ritikasandhu2 <ritika.betasoft@outlook.com>
2024-01-12 20:00:36 +0530

    fixed user password issue

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-use Hash;
+
 use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
@@ -17,18 +18,25 @@ class RegistrationController extends Controller
 
     public function  store(Request $request)
     {
-        $input = $request->all();
-        $response = User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => $input['password'],
-            'phone' => $input['phone'],
-        ]);
-        return response()->json([
-            'status' => true,
-            'message' => "Registation Success"
+        try {
 
+            $input = $request->all();
+            User::create([
+                'name' => $input['name'],
+                'email' => $input['email'],
+                'password' => Hash::make($input['password']),
+                'phone' => $input['phone'],
+            ]);
 
-        ]);
+            return response()->json([
+                'status' => true,
+                'message' => "Registation Success"
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
